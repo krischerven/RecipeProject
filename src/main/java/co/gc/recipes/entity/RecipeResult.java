cf6d57b5c6294a3aca6a76c89edf50b308f7241c
sean <seanhcyang10@gmail.com>
2019-09-05 10:31:04 -0400

    removed imports not used in reciperesults

--- src/main/java/co/gc/recipes/entity/RecipeResult.java
+++ src/main/java/co/gc/recipes/entity/RecipeResult.java
@@ -1,7 +1,7 @@
 package co.gc.recipes.entity;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
-import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RecipeResult {
